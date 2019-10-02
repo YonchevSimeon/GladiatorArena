151ef4e24de08cc32537245836df30205cfebff7
unknown <tromavoto@abv.bg>
2019-10-02 12:25:00 +0300

    Generated default Asp.Net Core 2.2 MVC project, removed some unnecessary files.

---
+++ src/GladiatorArena/GladiatorArena.Web/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace GladiatorArena.Web
+{
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args)
+                .Build()
+                .Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) 
+            => WebHost
+                .CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
